37f688d568d5e8ceab3a82f5b7672977dadfb0ac
Kyle Stapp <RabidCicada@gmail.com>
2015-04-28 08:20:18 -0400

    Working Indirect call harvesting and trace lookup for potential indirect call transitions

--- getstuff.py
+++ getstuff.py
@@ -4,7 +4,8 @@ import re
 
 funcmatcher = re.compile(r"^define.*@(.*)\(")
 blockmatcher = re.compile(r"^(\S+):")
-traceblockmatcher = re.compile(r"^        (\S+)\.(\S+),")
+traceblockmatcher = re.compile(r"^        \"([a-zA-Z_0-9]+)\.(\S+).*\.inst_[a-zA-Z_x0-9]+\",")
+traceaddrmatcher = re.compile(r"^        \"(0x[a-zA-Z0-9]+)\",")
 instmatcher = re.compile(r"^  [^ \t\n\r\f\v\]]\S*")
 callmatcher = re.compile(r"^  (call|tail call).*%\S+\(")
 #dstartblock = "block_0x807111f.i"
@@ -18,32 +19,49 @@ def findtransitions( tgtfunc, tgtblock, callstr, tracefilename):
 	currblock = None
 	nextcall = None
 	nextblock = None
+	address = None
 	lineidx = 1
 	transitions = []
+        print "Investigating for " + callstr + "<" + tgtfunc + "," + tgtblock + ">"
 	with open(tracefilename, 'r') as tfile:
 		for line in tfile:
 			match = traceblockmatcher.match(line)
 			if match is not None:
+                                #print "found func.block name " + line 
 				nextfunc = match.group(1)
 				nextblock = match.group(2)
+				address = None
 			else:
+                                #print "nonmatching " + line
+                                match = traceaddrmatcher.match(line)
+                                if match is not None:
+                                        address = match.group(1)
+                                else:
+                                        address = None
 				nextfunc = None
 				nextblock = None
 
-			if nextfunc is not None:
+			if nextfunc is not None and nextfunc != currfunc:
+                                #print "Checking " + str(nextfunc) + "." + str(nextblock) + " === " + line
 				#start once we found a likely callsite
 				if nextfunc == tgtfunc and nextblock == tgtblock:
+                                        print "Found Callsite Block " + nextfunc + "." + nextblock
 					currfunc = tgtfunc
-					nextblock = tgtblock
+					currblock = tgtblock
 	
 			if currblock is not None:
 				#look for transition out of function from callsite
 				if nextblock is None or nextblock != currblock:
 					#transition found
-					transitions.append(nextfunc + "." + nextblock + " [" + lineidx + "]")
- 
-			
+                                        print "Found transition " + line 
+					transitions.append( ((nextfunc + "." + nextblock) if address is None else address) + " [" + str(lineidx) + "]")
+					#print repr(transitions)
+					currblock = None
+					currfunc = None
+					address = None
+					
 			lineidx += 1
+			match = None
 	return transitions
 
 def processfiles( tracefilename, llvmfname, ofilename ):
@@ -61,6 +79,7 @@ def processfiles( tracefilename, llvmfname, ofilename ):
             match  = funcmatcher.match(line)
             if match is not None:
                 currfunction = match.group(1)
+                dfile.write(currfunction)
                 funcidx = 0
                 continue #get next line
 
@@ -80,15 +99,18 @@ def processfiles( tracefilename, llvmfname, ofilename ):
             
             if block is not None and instmatcher.match(line):
                 if callmatcher.match(line) is not None:
-                    dfile.write(line)
+                    #dfile.write(line)
+                    pass
                 #if debugactive:
                 #    if debugmatcher.match(line) is None:
                 #        dfile.write(line);
                 #if line == callstr:
                     ofile.write(currfunction+"::"+block+"::" + str(funcidx) + "::" + str(blockidx) + "-->"+line)
                     transitions = findtransitions(currfunction, block , line, tracefilename)
+                    print "Transitions are: " +  repr(transitions)
                     for item in transitions:
-			ofile.write("\t" + item)
+                            print "\t\t" + item + "\n"
+                            ofile.write("\t\t" + item + "\n")
                     continue
                 blockidx+=1
                 funcidx+=1

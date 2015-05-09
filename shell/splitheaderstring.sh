d10f4b9dc4e5f95e801382ab5e3ee832028fab2d
kyle.stapp <kyle.stapp@digitaloperatives.com>
2015-05-09 07:21:31 -0700

    Warning about needing special fold executable

--- shell/splitheaderstring.sh
+++ shell/splitheaderstring.sh
@@ -1,1 +1,2 @@
+#This requires a specially created version of fold that supports the -d<char> option (avoid breaking on character) it was created by kyle stapp to facilitate easy header file style split strings with fold.
 sed 's/^[[:space:]]*//' $1 | sed 's/[[:space:]]*$//' | sed -z 's/\n//g' | sed 's/"/\\"/g' | fold -w76 -d\\ |  sed 's/^/  "/g' | sed 's/$/"/g' > $2

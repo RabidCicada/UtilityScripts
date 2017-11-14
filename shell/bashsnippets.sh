0e6c76deb4e220a59196ff141cba459a50d5ee56
Kyle Stapp <kyle.stapp@starlab.io>
2017-11-14 10:56:45 -0500

    Fixing colors to match regular ubuntu

--- shell/bashsnippets.sh
+++ shell/bashsnippets.sh
@@ -1,6 +1,6 @@
 #title + green user@machine + blue path + regular $
 
 function settitle () {
-	  export PS1="\[\e]0;$1\a\]\[\033[32m\]\u@\h:\w\[\033[34m\]~\[\033[0m\]$"
+	  export PS1="\[\e]0;$1\a\]\[\033[1m\033[92m\]\u@\h:\[\033[94m\]\w\[\033[0m\]$"
 }
 
